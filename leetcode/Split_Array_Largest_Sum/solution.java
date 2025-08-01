a0db8fcb9584021c37ed9dce7aff8d9e0d28c9f1
Sabari M <151687270+sabarim6369@users.noreply.github.com>
2025-08-01 21:29:57 +0530

    Add solution for: Split Array Largest Sum

---
+++ leetcode/Split_Array_Largest_Sum/solution.java
@@ -0,0 +1,56 @@
+currsum=a;
+                count++;
+                
+            }
+            else{
+                currsum+=a;
+            }
+        }
+        return count<=k;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int left=*max_element(nums.begin(),nums.end());
+        int right=accumulate(nums.begin(),nums.end(),0);
+        int minans=INT_MAX;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(ispossible(mid,nums,k)){
+                minans=mid;
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+            
+        }
+        return minans;
+    }
+};
+                currsum=a;
+                count++;
+                
+            }
+            else{
+                currsum+=a;
+            }
+        }
+        return count<=k;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int left=*max_element(nums.begin(),nums.end());
+        int right=accumulate(nums.begin(),nums.end(),0);
+        int minans=INT_MAX;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(ispossible(mid,nums,k)){
+                minans=mid;
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+            
+        }
+        return minans;
+    }
+};
